2edbdbc5ce6bb4f06641857a8040957ae67d8787
BigJ <julienladeuze@outlook.fr>
2024-02-11 14:31:09 +0100

    changed dequeue

--- SegmentIntersector3000/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module be.ac.umons.firstg.segmentintersector {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens be.ac.umons.firstg.segmentintersector to javafx.fxml;
-    exports be.ac.umons.firstg.segmentintersector;
-}
