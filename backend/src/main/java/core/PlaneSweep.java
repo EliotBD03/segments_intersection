f9bb8c24baf96f47eff7f925bbd158fb61df1a3d
BigJ <julienladeuze@outlook.fr>
2024-03-22 08:57:32 +0100

    2/3 PlaneSweep.java done

--- backend/src/main/java/core/PlaneSweep.java
+++ backend/src/main/java/core/PlaneSweep.java
@@ -1,6 +1,7 @@
 package core;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class PlaneSweep
 {
@@ -29,13 +30,78 @@ public class PlaneSweep
         return intersections;
     }
 
-    public Point handleEventPoint(Point p)
+    public Point handleEventPoint(Point p) throws Exception
     {
         Point intersection = null;
-        ArrayList<Segment> upper = p.getStartOf();
-        ArrayList<Segment> lower = new ArrayList<>();
-        ArrayList<Segment> inner = new ArrayList<>();
-       //TODO statusQueue.findSegments
+        ArrayList<ComparableSegment> upper = p.getStartOf()
+                .stream()
+                .map(ComparableSegment::new)
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        ArrayList<ComparableSegment> lower = new ArrayList<>();
+        ArrayList<ComparableSegment> inner = new ArrayList<>();
+        statusQueue.findSegments(p,lower, inner);
+        ArrayList<ComparableSegment> lowerInner = union(lower, inner);
+        ArrayList<ComparableSegment> upperInner = union(lower, upper);
+        ArrayList<ComparableSegment> upperLowerInner = union(lowerInner, upper);
+        if(upperLowerInner.size() > 1)
+           //TODO intersection =
+        for(ComparableSegment segment : lowerInner)
+            statusQueue.remove(segment);
+        for(ComparableSegment segment : upperInner)
+            statusQueue.add(segment, p);
+        if(upperInner.isEmpty())
+        {
+            Pair<ComparableSegment, ComparableSegment> neighbours = statusQueue.getNeighbours(p);
+            if(neighbours.getItem1() != null && neighbours.getItem2() != null)
+                findNewEvent(neighbours.getItem1(), neighbours.getItem2(), p);
+        }
+        else
+        {
+            /// TODO
+        }
         return null;
+    }
+
+    public static ArrayList<ComparableSegment> union(ArrayList<ComparableSegment> l1, ArrayList<ComparableSegment> l2)
+    {
+        ArrayList<ComparableSegment> result = new ArrayList<>();
+        int j = 0;
+        int i = 0;
+        while(i < l1.size() || j < l2.size())
+        {
+            if(i >= l1.size())
+            {
+
+                i = l2.size();
+                result.addAll(l2.subList(i, i - 1));
+
+            }
+            else if(j >= l2.size())
+            {
+                j = l2.size();
+                result.addAll(l1.subList(j, l1.size() - 1));
+
+            }
+            else
+            {
+                if (l1.get(i).getUpperPoint().compareTo(l2.get(j).getUpperPoint()) <= 0) {
+                    result.add(l1.get(i));
+                    i++;
+                } else {
+                    result.add(l2.get(i));
+                    j++;
+                }
+            }
+        }
+        return result;
+    }
+
+    public void findNewEvent(ComparableSegment sl, ComparableSegment sr, Point p)
+    {
+        Point pp = Segment.findIntersection(sl, sr);
+        if(pp != null)
+            if(pp.compareTo(p) <= 0)
+                pointQueue.enqueue(p);
     }
 }
